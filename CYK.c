c69bfa889588c1e135ec261ef908be6729b3a121
Alexandre Quiterio <quiterio7@gmail.com>
2012-05-04 19:27:54 +0200

    Update CYK.c

--- CYK.c
+++ CYK.c
@@ -1,251 +1,156 @@
 /* 
  * File:   CYK.c
- * Author: alexandre
- *
+ * Author: alexandre, ist164714
  */
-
 #include <stdio.h>
-#include <stdlib.h>
-
 
 /*
  * DEFINE CONSTANTS
  */
 #define MARKER '#'
+#define ALPHABET 26
+
+/*
+ * DEFINE SYMBOL CONSTANTS
+ */
 #define NULL_SYMBOL '$'
 #define BASE_RULE 'A'
 #define INITIAL_RULE 'R'
-#define ALPHABET 26
+
+/*
+ * DEFINE RULE CONSTANTS
+ */
+#define MAX_RULES 1024
 #define MAX_SYMBOLS_PER_RULE 3
-#define MAX_BINARY_SEQUENCE 5000
 
-/* ************   GRAMMAR STRUCTS *********************** */
-/**
- * Struct Symbol has a character
- * and a position in array mode.
- * To find a symbol, we only need to
- * access the position in O(1)
+/*
+ * DEFINE SEQUENCES CONSTANTS
  */
-typedef struct Symbol {
-    char symbol;
-    int position;
-} Symbol;
+#define MAX_SEQUENCES 128
+#define MAX_BINARY_SEQUENCE 1024
 
-/**
- * Struct Rule has a head,
- * and a collection of terminal or non terminal symbols
- * If the Rule is a Terminal, it has the value
- * isTerminal marked with 1;
- */
-typedef struct Rule {
-    Symbol* head;
-    Symbol* terminator[2];
-    struct Rule *next;
-} Rule;
+/* ************   STACK INITIALIZATION *********************** */
 
-/**
- * List of Rules used on Grammar to define
- * Terminal or Non Terminal Rules
- */
-typedef struct RuleList {
-    Rule* first;
-    Rule* last;
-} RuleList;
-
-/**
- * Struct Grammar has two lists
- * of rules
- */
-typedef struct Grammar {
-    RuleList* NonTerminalRuleList;
-    RuleList* TerminalRuleList;
-} Grammar;
-
-/* ************   SEQUENCE STRUCTS *********************** */
-/**
- * Sequence has an array of characters
- * with MAX_BINARY_SEQUENCE size
- * and a pointer to the next element of the list
- */
-typedef struct Sequence {
-    char sequence[MAX_BINARY_SEQUENCE];
-    struct Sequence* next;
-} Sequence;
-/**
- * Struct SequenceCollection is a list
- * of sequences computed in the future
- * with CYK algorithm
- */
-typedef struct SequenceCollection {
-    Sequence* first;
-    Sequence* last;
-} SequenceCollection;
-
-/* ************   GRAMMAR CONSTRUCTORS *********************** */
-/**
- * Symbol Constructor
- */
-Symbol*
-newSymbol(char symbol) {
-    if(symbol == NULL_SYMBOL) {
-        return NULL;
-    }
-    Symbol* grammarSymbol = (Symbol*) malloc(sizeof(Symbol));
-    grammarSymbol->position = symbol - BASE_RULE;
-    grammarSymbol->symbol = symbol;
-    return grammarSymbol;
-}
-/**
- * Grammar Constructor
- */
-Grammar*
-newGrammar() {
-    Grammar *grammar = (Grammar*) malloc(sizeof(Grammar));
-    grammar->TerminalRuleList = (RuleList*) malloc(sizeof(RuleList));
-    grammar->NonTerminalRuleList = (RuleList*) malloc(sizeof(RuleList));
-    return grammar;
-}
-
-/**
- * Add Rule to List of rules
- */
-void
-addRuletoList(Rule* rule, RuleList* list) {
-    if(list->first != NULL) {
-        list->last->next = rule;
-    }
-    else {
-        list->first = rule;
-    }
-    list->last = rule;
-}
-/**
- * Add rule to Grammar
- */
-void
-addRuleToGrammar(Grammar* grammar, Rule* rule) {
-    if(rule->terminator[1] == NULL) {
-        addRuletoList(rule, grammar->TerminalRuleList);
-    } else  {
-        addRuletoList(rule, grammar->NonTerminalRuleList);
-    }
-}
-/**
- * Rule Constructor
- */
-Rule*
-newRule
-(Symbol* head, Symbol* terminator1, Symbol* terminator2) {
-
-    Rule *terminalRule = (Rule*) malloc(sizeof(Rule));
-    terminalRule->head=head;
-    terminalRule->terminator[0]=terminator1;
-    terminalRule->terminator[1]=terminator2;
-    terminalRule->next = NULL;
-
-    return terminalRule;
-}
-
-/* ************   SEQUENCE CONSTRUCTORS *********************** */
-/**
- * SequenceCollection Constructor
- */
-SequenceCollection*
-newSequenceCollection() {
-    SequenceCollection *collection =
-            (SequenceCollection*) malloc(sizeof(SequenceCollection));
-    collection->first = NULL;
-    collection->last = NULL;
-    return collection;
-}
-/**
- * AddNewSequence Constructor
- */
-void
-addNewSequence(SequenceCollection* collection, Sequence* sequence) {
-    if(collection->first != NULL) {
-        collection->last->next = sequence;
-    } else {
-        collection->first = sequence;
-    }
-    collection->last = sequence;
-}
-/**
- * newSequence Constructor
- */
-Sequence*
-newSequence() {
-    Sequence* newOne = (Sequence*) malloc(sizeof(Sequence));
-    newOne->next = NULL;
-    return newOne;
-}
+char collection[MAX_SEQUENCES][MAX_BINARY_SEQUENCE];
+char grammarNT[MAX_RULES][MAX_SYMBOLS_PER_RULE];
+char grammarT[ALPHABET*2][MAX_SYMBOLS_PER_RULE - 1];
+int nTerminals = 0;
+int nnoTerminals = 0;
+int Nsequences = 0;
 
 /* ************   PARSING FUNCTIONS *********************** */
 /**
  * strlen: String Length function Optimization
  * @param receives a char pointer
- * @return the length of the pointer minus one (\0)
+ * @return the length of the pointer with \0
  */
 size_t
-strlen(str) const char *str; {
+strlen(str) const char *str; 
+{
   const char *s;
   for (s = str; *s; ++s);
-  return(s - str-1);
+  return(s - str);
 }
 /**
- * GetRule: Read one rule of input
+ * GetRule: Read one rule from input
  * @param line[] receives an array of chars
- * @return the same array formated to be consumed
- * by rule generator
+ * @return the same array formated is returned 
+ * to be consumed by rule generator
  */
 void
-getRule(char line[]) {
+getRule(line) char line[]; 
+{
     int length;
-    fgets(line, sizeof(line), stdin);
-    length = strlen(line);
+    char* check;
+    check = fgets(line, sizeof(line)*MAX_SYMBOLS_PER_RULE, stdin);
+    length = strlen(line) - 1;
     if(length == 2) {
         line[2] = NULL_SYMBOL;
     }
 }
+
 /**
- * ReadRules: Read all the rules used in Grammar to parse
- * analyses the sequences in the future
- * @param grammar the grammar used to construct
- * the Rule Domain
- * @return void
+ * ReadRules: Read all the rules used in Grammar
+ * @param the empty dictionary of rules
+ * @return the grammar completed with all the rules
+ * read from stdin
  */
 void
-readRules(Grammar* grammar) {
-    Rule* rule = NULL;
+readRules() 
+{
     char bucket[MAX_SYMBOLS_PER_RULE];
-
     getRule(bucket);
-    while (bucket[0] != MARKER) {
-        rule = newRule( newSymbol(bucket[0]),
-                        newSymbol(bucket[1]),
-                        newSymbol(bucket[2]));
-        addRuleToGrammar(grammar, rule);
+    while (bucket[0] != MARKER) 
+    {
+        if(bucket[2] == NULL_SYMBOL) 
+        {
+            grammarT[nTerminals][0] = bucket[0];
+            grammarT[nTerminals][1] = bucket[1];
+            nTerminals++;
+        } 
+        else 
+        {
+            grammarNT[nnoTerminals][0] = bucket[0];
+            grammarNT[nnoTerminals][1] = bucket[1];
+            grammarNT[nnoTerminals][2] = bucket[2];
+            nnoTerminals++;
+        }
         getRule(bucket);
     }
 }
 
-/**
- * Read all the sequences used to print the problem final
- * solution
- * @param SequenceCollection    a struct with all the sequences
- * @return void
- */
-void
-readSequences (SequenceCollection* collection) {
-    Sequence* newSeq = newSequence();
-    fgets(newSeq->sequence, sizeof(newSeq->sequence), stdin);
+void 
+getSequence(char line[])
+{
+    char* check;
+    check = fgets(line, sizeof(line)*MAX_BINARY_SEQUENCE, stdin);
+}
 
-    while(newSeq->sequence[0] != MARKER) {
-        addNewSequence(collection, newSeq);
-        newSeq = newSequence();
-        fgets(newSeq->sequence, sizeof(newSeq->sequence), stdin);
+/**
+ * ReadSequences: Read all the sequences used to 
+ * compute and decide if sequence belongs to the grammar language or not
+ * @param SequenceCollection  an empty dictionary with space to put their a 
+ * maximum of sequences of 1024 numbers
+ * @return the collection with all the sequences parsed from input
+ */ 
+void
+readSequences() 
+{
+    getSequence(collection[Nsequences]);
+    while(collection[Nsequences][0] != MARKER) 
+    {
+        Nsequences++;
+        getSequence(collection[Nsequences]);
     }
-    
+}
+
+/* ************   BIT SHIFTING FUNCTIONS *********************** */
+
+/**
+ * PutItOne: mark a position in bit array to one
+ * @param position  the position which we want to mark 
+ * @param number the old value 
+ * @return the new value with position p marked
+ */ 
+inline void 
+putItOne(int position, unsigned int* number)
+{
+    unsigned int buff = 1 << position;
+    *number = (*number | buff);
+}
+
+/**
+ * GetValue: if the value in position p is marked with one, then return 1
+ * otherwise return 0
+ * @param position  the position which we want to check
+ * @param const value the value of the search
+ * @return 1 if position p is marked with 1, otherwise return 0
+ */ 
+inline int 
+getValue(int position, int const value) 
+{
+    unsigned int buff = 1 << position;
+    return (value & buff);
 }
 
 /* ************   CYK Algorithm *********************** */
@@ -258,47 +163,53 @@ readSequences (SequenceCollection* collection) {
  * @return  void
  */
 void
-cykAlgorithm(Grammar* grammar, Sequence* sequence) {
+cykAlgorithm(sequenceNumber) int sequenceNumber;
+{
 
-    int n = strlen(sequence->sequence)+1, ix, jx, kx;
-    Rule* rule = NULL;
-    int matrix[n][n][ALPHABET];
-    
-        
-    for(ix = 0; ix < n; ix++) {
-        for(jx = 0; jx < n; jx++) {
-                for(kx = 0; kx < ALPHABET; kx++) {
-                    matrix[ix][jx][kx] = 0;
-                }
+    int n = strlen(collection[sequenceNumber]), ix, jx, kx, dx;
+    unsigned int matrix[n][n];
+
+   for(ix = 1; ix < n; ix++) 
+   {
+        for(jx = 1; jx < n; jx++) 
+        {
+            matrix[ix][jx]= 0;
         }
     }
+
     // Frist Iteration Bottom-Up
-    for(ix = 1; ix < n; ix++) {
-        rule = grammar->TerminalRuleList->first;
-        for(; rule != NULL;) {
-            if(rule->terminator[0]->symbol == sequence->sequence[ix-1]) {
-                matrix[ix][1][rule->head->position] = 1;
+    for(ix = 1; ix < n; ix++) 
+    {
+        for(jx = 0; jx < nTerminals; jx++) 
+        {
+            if(grammarT[jx][1] == collection[sequenceNumber][ix-1]) 
+            {
+                putItOne(grammarT[jx][0]- BASE_RULE, &matrix[ix][1]);
             }
-            rule = rule->next;
         }
     }
+    
     //Main Loop of Algorithm
-    for(ix = 2; ix < n; ix++) {
-        for(jx = 1; jx < n - ix + 1; jx++) {
-            for(kx = 1; kx < ix; kx++) {
-                rule = grammar->NonTerminalRuleList->first;
-                for(; rule != NULL;) {
-                    if((matrix[jx][kx][rule->terminator[0]->position] == 1)
-                    && (matrix[jx + kx][ix-kx][rule->terminator[1]->position] == 1)) {
-                        matrix[jx][ix][rule->head->position] = 1;
-                    }
-                    rule = rule->next;
+    for(ix = 2; ix < n; ix++) 
+    {
+        for(jx = 1; jx < n - ix + 1; jx++) 
+        {
+            for(kx = 1; kx < ix; kx++) 
+            {
+                for(dx = 0; dx < nnoTerminals; dx++) 
+                {
+                    if(getValue(grammarNT[dx][1]- BASE_RULE, matrix[jx][kx]) 
+                        && getValue(grammarNT[dx][2]- BASE_RULE, matrix[jx + kx][ix-kx])) 
+                        {
+                            putItOne(grammarNT[dx][0]- BASE_RULE, &matrix[jx][ix]);
+                        }
                 }
             }
         }
     }
     
-    if(matrix[1][n-1][INITIAL_RULE-BASE_RULE] == 1) {
+    if(getValue(INITIAL_RULE-BASE_RULE, matrix[1][n-1])) 
+    {
         printf("yes\n");
         return;
     }
@@ -316,28 +227,29 @@ cykAlgorithm(Grammar* grammar, Sequence* sequence) {
  * @return void
  */
 void
-solveProblem(Grammar* grammar, SequenceCollection* collection) {
-    Sequence* sequence = collection->first;
-    if(sequence == NULL) {
+solveProblem() 
+{
+
+    int computedSequences = 0;
+    if(nTerminals == 0 || Nsequences == 0) 
+    {
         return;
     }
-    while(sequence != NULL) {
-        cykAlgorithm(grammar,sequence);
-        sequence = sequence->next;
+
+    while(computedSequences < Nsequences) 
+    {
+        cykAlgorithm(computedSequences);
+        computedSequences++;
     }
 }
 
 /**
  * Main Function
  */
-int main(void) {
-
-    Grammar * grammar = newGrammar();
-    SequenceCollection *collection = newSequenceCollection();
-
-    readRules(grammar);
-    readSequences(collection);
-    solveProblem(grammar, collection);
-
-     return 0;
+int main(void) 
+{
+    readRules();
+    readSequences();
+    solveProblem();
+    return 0;
 }
