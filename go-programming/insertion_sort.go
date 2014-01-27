3739ab112c33d6663dab260a4f11e35ebca0fa56
Alexandre Quiterio <quiterio7@gmail.com>
2014-01-27 21:55:31 +0000

    organize files

---
+++ go-programming/insertion_sort.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * Insertion Sort Algorithm
+ */
+func sort(array []int) {
+	var key int
+	var j int
+	for i := 1; i < len(array); i++ {
+		key = array[i]
+		j = i
+		for j > 0 && array[j-1] > key {
+			array[j] = array[j-1]
+			j = j - 1
+		}
+		array[j] = key
+	}
+	fmt.Println(array)
+}
+
+func main() {
+	sort([]int{1, 5, 3, 4, 6, 7, 8})
+	sort([]int{2, 5, 1, 2, 3, 4, 7, 6})
+}
