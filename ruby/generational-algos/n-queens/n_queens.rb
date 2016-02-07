dd4a68f89aa54aaa26c65e22ef879a7c31786292
Alex Quiterio <afp.quiterio@gmail.com>
2016-02-07 11:38:55 +0000

    update gen algo

--- ruby/generational-algos/n-queens/n_queens.rb
+++ ruby/generational-algos/n-queens/n_queens.rb
@@ -2,9 +2,9 @@ class Solver
   attr_reader :population
   def initialize(options)
     @population      = []
-    @decimation      = options.fetch(:decimation)
-    @population_size = options.fetch(:population_size)
     @max_generations = options.fetch(:max_generations)
+    @decimation      = options.fetch(:decimation, 0.4)
+    @population_size = options.fetch(:size, 100)
   end
 
   def best(population)
@@ -24,11 +24,11 @@ class Solver
   end
 
   def create_first_generation(population_size)
-    population_size.times.map { gen_random_gene }
+    population_size.times.map { _create_gene(gen_random_gene) }
   end
 
   def _sort_population(population)
-    population.sort { |a,b| b.fitness - a.fitness }
+    population.sort { |a,b| b[:fitness] - a[:fitness] }
   end
 
   def _decimate(population, decimation)
@@ -48,13 +48,13 @@ class Solver
   end
 
   def _select(population)
-    population[(population.length * gen_random_gene).floor]
+    population[(population.length * rand).floor]
   end
 end
 
 class NQueens < Solver
   def gen_random_gene
-    gene = Array.new(@board_size);
+    gene = Array.new(@population_size);
     gene.size.times do |i|
       gene[i] = i
     end
@@ -68,11 +68,11 @@ class NQueens < Solver
   def reproduce(geneA, geneB)
     new_gene   = ([false] * geneA[:gene].size)
     _next      = (rand * new_gene.size).floor
-    while !new_gene[_next]
+    while new_gene[_next] == false
       new_gene[_next] = geneA[:gene][_next]
       _next = geneA[:gene].find_index do |crox|
         break if crox == geneB[:gene][_next]
-      end
+      end || (new_gene.length - 1)
     end
     geneB[:gene].each_with_index do |ix, crox|
       new_gene[ix] ||= crox
@@ -80,6 +80,22 @@ class NQueens < Solver
     return new_gene
   end
 
-  def evaluate
+  def evaluate(gene)
+    threats = 0
+    gene.size.times do |i|
+      gene.size.times do |j|
+        threats += 1 if is_threat(gene, i,j)
+      end
+    end
+    max_threats = gene.size * (gene.size - 1)
+    return (max_threats - threats) / max_threats
+  end
+
+  def is_threat(gene, i, j)
+    return false if i == j
+    return true if gene[i] == gene[j]
+    former = [i, j].min
+    latter = [i, j].max
+    return (gene[latter] - gene[former]) == (latter - former)
   end
 end
